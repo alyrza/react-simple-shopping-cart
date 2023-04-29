93d40a3af00e6a0b5b9acbd853fa79749a8f03d4
alyrza <alyrzanm@gmail.com>
2023-04-29 21:07:30 +0430

    Add product component

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,73 +1,34 @@
 import React, { Component } from "react";
 import "./Main.css";
+import Product from "./Product/Product";
 
 class Main extends Component {
+  constructor() {
+    super();
+    this.state = {
+      products: [
+        { id: 1, title: "ساعت چرمی", price: "51000", image: "product-images/1.webp" },
+        { id: 2, title: "خردکن سریع", price: "25000", image: "product-images/2.webp" },
+        { id: 3, title: "محافظ ولتاژ", price: "86300", image: "product-images/3.webp" },
+        { id: 4, title: "تونیک زنانه", price: "12600", image: "product-images/4.webp" },
+        { id: 5, title: "کتاب های خوب", price: "33200", image: "product-images/5.webp" },
+        { id: 6, title: "کیف مدرسه", price: "14600", image: "product-images/6.webp" },
+        { id: 7, title: "مایع دستشویی", price: "87100", image: "product-images/7.webp" },
+        { id: 8, title: "شلوار زیبا", price: "63000", image: "product-images/8.webp" },
+        { id: 9, title: "کتاب زندگی خود", price: "12000", image: "product-images/9.webp" },
+        { id: 10, title: "جعبه ابزار", price: "13000", image: "product-images/10.webp" },
+      ],
+    };
+  }
   render() {
     return (
       <main>
         <section>
-          <h2 class="title">محصولات</h2>
-          <div class="products">
-            <div class="product">
-              <img src="product-images/1.webp" alt="" />
-              <h3 class="product-title">کفش راحتی</h3>
-              <h3 class="product-price">قیمت : 25000 تومان</h3>
-              <button class="product-btn">افزاودن به سبد خرید</button>
-            </div>
-            <div class="product">
-              <img src="product-images/2.webp" alt="" />
-              <h3 class="product-title">کفش راحتی</h3>
-              <h3 class="product-price">قیمت : 25000 تومان</h3>
-              <button class="product-btn">افزاودن به سبد خرید</button>
-            </div>
-            <div class="product">
-              <img src="product-images/3.webp" alt="" />
-              <h3 class="product-title">کفش راحتی</h3>
-              <h3 class="product-price">قیمت : 25000 تومان</h3>
-              <button class="product-btn">افزاودن به سبد خرید</button>
-            </div>
-            <div class="product">
-              <img src="product-images/4.webp" alt="" />
-              <h3 class="product-title">کفش راحتی</h3>
-              <h3 class="product-price">قیمت : 25000 تومان</h3>
-              <button class="product-btn">افزاودن به سبد خرید</button>
-            </div>
-            <div class="product">
-              <img src="product-images/5.webp" alt="" />
-              <h3 class="product-title">کفش راحتی</h3>
-              <h3 class="product-price">قیمت : 25000 تومان</h3>
-              <button class="product-btn">افزاودن به سبد خرید</button>
-            </div>
-            <div class="product">
-              <img src="product-images/6.webp" alt="" />
-              <h3 class="product-title">کفش راحتی</h3>
-              <h3 class="product-price">قیمت : 25000 تومان</h3>
-              <button class="product-btn">افزاودن به سبد خرید</button>
-            </div>
-            <div class="product">
-              <img src="product-images/7.webp" alt="" />
-              <h3 class="product-title">کفش راحتی</h3>
-              <h3 class="product-price">قیمت : 25000 تومان</h3>
-              <button class="product-btn">افزاودن به سبد خرید</button>
-            </div>
-            <div class="product">
-              <img src="product-images/8.webp" alt="" />
-              <h3 class="product-title">کفش راحتی</h3>
-              <h3 class="product-price">قیمت : 25000 تومان</h3>
-              <button class="product-btn">افزاودن به سبد خرید</button>
-            </div>
-            <div class="product">
-              <img src="product-images/9.webp" alt="" />
-              <h3 class="product-title">کفش راحتی</h3>
-              <h3 class="product-price">قیمت : 25000 تومان</h3>
-              <button class="product-btn">افزاودن به سبد خرید</button>
-            </div>
-            <div class="product">
-              <img src="product-images/10.webp" alt="" />
-              <h3 class="product-title">کفش راحتی</h3>
-              <h3 class="product-price">قیمت : 25000 تومان</h3>
-              <button class="product-btn">افزاودن به سبد خرید</button>
-            </div>
+          <h2 className="title">محصولات</h2>
+          <div className="products">
+            {this.state.products.map((product) => {
+              return <Product {...product} key={product.id} />;
+            })}
           </div>
         </section>
       </main>
---
+++ src/Main/Product/Product.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Product extends Component {
+  render() {
+    return (
+      <div className="product">
+        <img src={this.props.image} alt="" />
+        <h3 className="product-title">{this.props.title}</h3>
+        <h3 className="product-price">قیمت : {this.props.price} تومان</h3>
+        <button className="product-btn">افزودن به سبد خرید</button>
+      </div>
+    );
+  }
+}
+
+export default Product;
